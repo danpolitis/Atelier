ba0f7b11baee0421fd552ee372fc43cfb1100075
Andrew Hang <82345254+DrewHang@users.noreply.github.com>
2021-09-11 17:29:14 -0700

    Added star rating component (#19)

---
+++ client/src/components/Review-Components/StarRating.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import StarRatings from 'react-star-ratings';
+import axios from 'axios';
+
+function StarRating({ productId }) {
+  const [average, setAverage] = useState(0);
+
+  const getReviews = (id) => {
+    axios.get(`/api/reviews?product_id=${id}`)
+      .then(({ data }) => {
+        const averageReview = data.results.map(item => item.rating)
+          .reduce((prev, curr) => prev + curr);
+        setAverage(averageReview / data.results.length);
+      })
+      .catch(err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+  };
+
+  useEffect(() => {
+    getReviews(productId);
+  }, []);
+
+  return (
+    <div>
+      <StarRatings rating={average} starRatedColor="rgb(253,204,13)" numberOfStars={5} starDimension="30px" />
+    </div>
+
+  );
+}
+
+export default StarRating;
